8d1ee539480758161cf8e1766545925fa5b4487b
Shree Thakre <shreet365@gmail.com>
2023-02-12 04:56:29 +0530

    Adding Part Time Emplyee & Wage

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,15 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Welcome to Employee Wage Computation ");
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
+            int IS_PART_TIME = 1;        
+            int IS_FULL_TIME = 2;       
+            int EPM_RATE_PER_HOUR = 20;    
 
-            int empHrs = 0;
-            int empWage = 0;
+            int empHrs = 0;       
+            int empWage = 0;      
+
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
@@ -20,9 +26,8 @@
             {
                 empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Daily Employee Wage is: {0}", empWage);
-
+            empWage = empHrs * EPM_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage is: {0}", empWage);
             Console.ReadLine();
         }
     }
