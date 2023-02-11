74d8a2631642a121766408180ff4a01500aaea57
Shree Thakre <shreet365@gmail.com>
2023-02-12 04:45:51 +0530

    Check Emplyee Pesent or Not

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Employee Wage Computation ");
+            // Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+
+            //Coputaion
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Not Present");
+            }
             Console.ReadLine();
         }
     }
